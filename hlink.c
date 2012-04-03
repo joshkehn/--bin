b7e77e8f7943e81489d37824f66568616dcdb4e1
Joshua Kehn <josh.kehn@gmail.com>
2012-04-03 00:16:43 -0400

    Added a comment

--- hlink.c
+++ hlink.c
@@ -3,6 +3,7 @@
 
 int main (int argc, char* argv[])
 {
+    // First check to make sure that there are three arguments
     if (argc != 3)
     {
         printf("usage: hlink [source] [target]\n");
